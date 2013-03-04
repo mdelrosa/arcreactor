c5ef40b19a7e4debee1295840aabb2fbb4b1c437
Joshua Langowitz <jlangowitz@Garrus.(none)>
2013-03-03 20:35:30 -0500

    dealing with merge conflicts

--- routes/index.js
+++ routes/index.js
@@ -16,9 +16,8 @@ exports.index = function(req, res){
   for (i=0;i<10;i++) {
   	counts.push(Math.floor(Math.random()*10))
   }
-<<<<<<< HEAD
-  res.render('homepage', { title: 'Arc Reactor', counts: counts, Songs:""});
-=======
+  // res.render('homepage', { title: 'Arc Reactor', counts: counts, Songs:""});
+
   echo('song/search').get({
     artist: 'taylor swift',
     title: 'trouble',
@@ -28,5 +27,4 @@ exports.index = function(req, res){
     console.log(json.response.songs.title);
   });
   res.render('index', { title: 'ArcReactor', counts: counts, APIkey: echoAPI});
->>>>>>> 96136f5e38e3ce4b9eb53c2c6e254c6a59517d43
 };
