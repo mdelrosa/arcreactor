d61980a318acf379ea5725b64624da696ef01e1a
Doyung <doyung@doyung-VirtualBox.(none)>
2013-03-03 20:27:55 -0500

    Added the logo

--- routes/index.js
+++ routes/index.js
@@ -8,5 +8,5 @@ exports.index = function(req, res){
   for (i=0;i<10;i++) {
   	counts.push(Math.floor(Math.random()*10))
   }
-  res.render('homepage', { title: 'ArcReactor', counts: counts, Songs:""});
+  res.render('homepage', { title: 'Arc Reactor', counts: counts, Songs:""});
 };
